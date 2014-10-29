80129a7b3a3ec92d1d31c0fe04d999d0a74d793f
rchavarria <txingo@gmail.com>
2014-10-30 00:53:39 +0100

    Add comments to explain .then() and .catchError() methods, and .then() chaining

--- futures/src/futures3.dart
+++ futures/src/futures3.dart
@@ -4,8 +4,15 @@ import 'dart:async';
 void printDailyNewsDigest() {
   File file = new File("dailyNewsDigest.txt");
   Future future = file.readAsString();
+  // call doSomethingWith content if the file is read
   future.then((content) => doSomethingWith(content))
+	// handleError if something was wrong
         .catchError((e) => handleError(e));
+  // notice that calls are chained with only one `.`
+  // if two dots were used, `..`, the catchError method
+  // will be of the future object, and with one `.` the
+  // catchError method is of the Future object return by
+  // the `then()` method
 }
 
 void main() {
--- futures/src/futures4.dart
+++ futures/src/futures4.dart
@@ -2,11 +2,16 @@ import 'dart:io';
 import 'dart:async';
 
 void main() {
+  // chain differen futures. when the future A is resolve, if calls
+  // the callback A, who returns the value returned by future B (`expensiveB()`)
+  // Then, the callback B is chained and called when that future is
+  // is resolved, that returns the last future
   expensiveA().then((aValue) => expensiveB()) 
               .then((bValue) => expensiveC()) 
               .then((cValue) => doSomethingWith(cValue));
 }
 
+// create three different futures with new Future.value()
 Future expensiveA() => new Future.value('from expensiveA');
 Future expensiveB() => new Future.value('from expensiveB');
 Future expensiveC() => new Future.value('from expensiveC');
