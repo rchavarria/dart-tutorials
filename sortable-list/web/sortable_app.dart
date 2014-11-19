33bb809aa6e8b995c2d37bdd00e05b3be61c916f
rchavarria <txingo@gmail.com>
2014-11-20 00:37:30 +0100

    Add sortable-app web component

---
+++ sortable-list/web/sortable_app.dart
@@ -0,0 +1,16 @@
+import 'package:polymer/polymer.dart';
+
+@CustomTag('sortable-app')
+class SortableApp extends PolymerElement {
+  @observable bool applyAuthorStyles = true;
+
+  List<String> tasks = const [
+    'Import polymer dart library',
+    'Annotate with @CustomeTag',
+    'extends from PolymerElement',
+    'observe some variables',
+    'Define a named constructor .created()'
+    ];
+
+  SortableApp.created() : super.created();
+}
