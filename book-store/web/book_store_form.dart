3c581f859c59c5f4be531c05c7748806f3f8d87a
rchavarria <txingo@gmail.com>
2014-12-05 00:49:04 +0100

    Implement Dart code for the form

---
+++ book-store/web/book_store_form.dart
@@ -0,0 +1,27 @@
+import 'dart:html';
+import 'dart:convert';
+import 'package:polymer/polymer.dart';
+
+@CustomTag('book-store-form')
+class BookStoreForm extends FormElement with Polymer, Observable {
+  
+  SlamBookComponent.created() : super.created() { 
+    polymerCreated();
+  }
+
+  void submitForm(Event e, var detail, Node target) {
+    e.preventDefault(); // Don't do the default submit.
+       
+    HttpRequest request = new HttpRequest();
+    
+    // POST the data to the server.
+    var url = 'http://127.0.0.1:4040';
+    request.open('POST', url);
+    request.send(buildDataToBeSent());
+  }
+  
+  String buildDataToBeSent() {
+    return JSON.encode({'foo': 'bar'});
+  }
+}
+
