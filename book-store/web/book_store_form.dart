7dd843e2353984d3d80aed732fba5f39e541d1cb
rchavarria <txingo@gmail.com>
2014-12-05 01:05:36 +0100

    Fix some typos to get all parts working

--- book-store/web/book_store_form.dart
+++ book-store/web/book_store_form.dart
@@ -4,24 +4,26 @@ import 'package:polymer/polymer.dart';
 
 @CustomTag('book-store-form')
 class BookStoreForm extends FormElement with Polymer, Observable {
-  
-  SlamBookComponent.created() : super.created() { 
-    polymerCreated();
-  }
 
-  void submitForm(Event e, var detail, Node target) {
-    e.preventDefault(); // Don't do the default submit.
-       
-    HttpRequest request = new HttpRequest();
-    
-    // POST the data to the server.
-    var url = 'http://127.0.0.1:4040';
-    request.open('POST', url);
-    request.send(buildDataToBeSent());
-  }
-  
-  String buildDataToBeSent() {
-    return JSON.encode({'foo': 'bar'});
-  }
+    @observable String someText = 'Foo bar';
+
+    BookStoreForm.created() : super.created() { 
+        polymerCreated();
+    }
+
+    void submitForm(Event e, var detail, Node target) {
+        e.preventDefault(); // Don't do the default submit.
+           
+        HttpRequest request = new HttpRequest();
+
+        // POST the data to the server.
+        var url = 'http://127.0.0.1:4040';
+        request.open('POST', url);
+        request.send(buildDataToBeSent());
+    }
+
+    String buildDataToBeSent() {
+        return JSON.encode({'foo': 'bar'});
+    }
 }
 
