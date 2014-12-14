5218a9a9e8ac72bf4e13bc0952b692a72a906c9c
rchavarria <txingo@gmail.com>
2014-12-15 00:40:00 +0100

    Check number of arguments

--- reading-files/bin/read.dart
+++ reading-files/bin/read.dart
@@ -2,5 +2,11 @@ import 'dart:io';
 import 'dart:convert';
 
 void main(List<String> arguments) {
-    print('how many arguments? ${arguments.length}');
+    if (arguments.length != 1) {
+        print('Usage: dart bin/read.dart <filename>');
+        return;
+    }
+
+    String filePath = arguments[0];
+    print('You are gonna read the file ${filePath}');
 }
