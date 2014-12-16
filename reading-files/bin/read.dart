a8b6f1c91e8a6d6e756412b3980892f441851aac
rchavarria <txingo@gmail.com>
2014-12-17 00:41:38 +0100

    Switch checking conditions to verify first that a file is not a directory

--- reading-files/bin/read.dart
+++ reading-files/bin/read.dart
@@ -15,30 +15,30 @@ void main(List<String> arguments) {
 void readFile(String filePath) {
     File file = new File(filePath);
 
-    file.exists()
-    .then((fileExists) {
-        if (!fileExists) {
-            return new Future.error('File ${filePath} does not exists');
-        }
+    FileSystemEntity.isDirectory(filePath)
+        .then((pathIsDirectory) {
+            if (pathIsDirectory) {
+                return new Future.error('${filePath} is a directory and can not be read');
+            }
 
-        return new Future.value(filePath);
-    })
-    .then((_) => FileSystemEntity.isDirectory(filePath))
-    .then((pathIsDirectory) {
-        if (pathIsDirectory) {
-            return new Future.error('${filePath} is a directory and can not be read');
-        }
+            return new Future.value();
+        })
+        .then((_) => file.exists())
+        .then((fileExists) {
+            if (!fileExists) {
+                return new Future.error('File ${filePath} does not exists');
+            }
 
-        return new Future.value();
-    })
-    .then((_) {
-        print('Finally, reading file ${filePath}');
-        file.openRead()                     // returns a Stream to listen to
-            .transform(UTF8.decoder)        // transforms int's to characters
-            .transform(new LineSplitter())  // split in lines
-            .listen((line) {                // listen each line
-                print(line);
-            });
-    });
+            return new Future.value(filePath);
+        })
+        .then((_) {
+            print('Finally, reading file ${filePath}');
+            file.openRead()                     // returns a Stream to listen to
+                .transform(UTF8.decoder)        // transforms int's to characters
+                .transform(new LineSplitter())  // split in lines
+                .listen((line) {                // listen each line
+                    print(line);
+                });
+        });
 }
 
